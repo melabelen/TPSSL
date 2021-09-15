f8498cef32ee0f66e2a3e3f0136d023e39322aac
Melany Holweger <melany.holweger@clara.co>
2021-09-15 12:55:03 -0300

    JS works for 1-10 decimals

--- obtenerNotacion.js
+++ obtenerNotacion.js
@@ -1,56 +1,96 @@
 const obtenerNotacion = (palabraAnalizada) => {
-    let i = 0;
-    let notacion = "";
-    let numeroActual = "";
-    let operadorEnEspera = ""; 
-    // analizo la operación que ya sé que es válida
-    while(palabraAnalizada.length > i){
-          console.log(palabraAnalizada[i])
-          //no es un operador, es un numero y obtengo todos sus digitos;
-          if(palabraAnalizada[i] != '+' && palabraAnalizada[i] != '*' && palabraAnalizada[i] != '-'){
-              numeroActual = numeroActual + palabraAnalizada[i]
-              i = i +1;
+  let i = 0;
+  let notacion = "";
+  let numeroActual = "";
+  let operadorEnEspera = ""; 
+  // analizo la operación que ya sé que es válida
+  while(palabraAnalizada.length > i){
+        //no es un operador, es un numero y obtengo todos sus digitos;
+        if(palabraAnalizada[i] != '+' && palabraAnalizada[i] != '*' && palabraAnalizada[i] != '-'){
+            numeroActual = numeroActual + palabraAnalizada[i]
+            i = i +1;
+        }
+        //es un operador
+        else{
+          //como es un operador, el numero que lo precede ya es parte de la notacion
+          notacion = notacion + numeroActual;
+          //reseteo el numeroActual
+          numeroActual = "";
+          //El operador es de los de menor prioridad
+          if(operadorEnEspera == "+" || operadorEnEspera == '-'){
+            //verifico si el operador actual tiene mayor prioridad
+              if(palabraAnalizada[i] == '*'){
+                //la notacion toma al operador de mayor prioridad y el de menor
+                //sigue en espera
+                operadorEnEspera = '*'+operadorEnEspera
+              }
+              else{
+                //el operador en espera pasa a ser parte de la notacion 
+                //el operador analizado actualmente pasa a estar en espera
+                notacion = notacion + operadorEnEspera;
+                operadorEnEspera = palabraAnalizada[i];
+              }
           }
-          //es un operador
+          //el operador es de mayor prioridad, el caracter vacio o
+          //un conjunto de operadores
           else{
-            //como es un operador, el numero que lo precede ya es parte de la notacion
-            notacion = notacion + numeroActual;
-            //reseteo el numeroActual
-            numeroActual = "";
-            //El operador es de los de menor prioridad
-            if(operadorEnEspera == "+" || operadorEnEspera == '-'){
-              //verifico si el operador actual tiene mayor prioridad
-                if(palabraAnalizada[i] == '*'){
-                  //la notacion toma al operador de mayor prioridad y el de menor
-                  //sigue en espera
-                  operadorEnEspera = '*'+operadorEnEspera
-                }
-                else{
-                  //el operador en espera pasa a ser parte de la notacion 
-                  //el operador analizado actualmente pasa a estar en espera
-                  notacion = notacion + operadorEnEspera;
-                  operadorEnEspera = palabraAnalizada[i];
-                }
-            }
-            //el operador es de mayor prioridad, el caracter vacio o
-            //un conjunto de operadores
-            else{
-               notacion = notacion  + operadorEnEspera;
-                operadorEnEspera = palabraAnalizada[i]
-            }
-            i = i + 1;
+             notacion = notacion  + operadorEnEspera;
+              operadorEnEspera = palabraAnalizada[i]
           }
-    }
-    //guardo lo que quedó en las variables al terminar el loop
-    notacion = notacion + numeroActual + operadorEnEspera
-    
-    return notacion;
-    
+          i = i + 1;
+        }
   }
+  //guardo lo que quedó en las variables al terminar el loop
+  notacion = notacion + numeroActual + operadorEnEspera
   
-  console.log(obtenerNotacion("3*2+5*3-2*6"))
+  return notacion;
+  
+}
 
 
-  //para correr el codigo: https://onecompiler.com/javascript
+const resolverNotacion = (notacion) => {
+  // La pila que va a ir resolviendo las operaciones
+  const stack = [];
+  // Iterador
+  let i = 0;
+  let caracterActual = "";
+  let operador1 = "";
+  let operador2 = "";
+  let resultadoParcial = "";
+  // recorro la palabra
+  while(notacion.length > i){
+    //si es un numero, lo agrego al stack
+    if(notacion[i] != '+' && notacion[i] != '*' && notacion[i] != '-'){
+      stack.push(notacion[i]);
+    }
+    //si no es un numero, opero 
+    else{
+      operador1 = stack.pop();
+      operador2 = stack.pop();
+      if(notacion[i] == '+'){
+        resultadoParcial = parseInt(operador1) + parseInt(operador2);
+      }
+      if(notacion[i] == '-'){
+        resultadoParcial = parseInt(operador2) - parseInt(operador1);
+      }
+      if(notacion[i] == '*'){
+        resultadoParcial = parseInt(operador1) * parseInt(operador2);
+      }
+      stack.push(resultadoParcial);
+        
+    }
+    i = i + 1;
+  }
+  return stack[0];
+}
 
-  // para corroborar las notaciones: https://www.dcode.fr/reverse-polish-notation
+
+//junto ahre
+const operacion = "3+4*7+3-5";
+const resultado = resolverNotacion(obtenerNotacion(operacion))
+console.log(resultado);
+
+
+//para correr el codigo: https://onecompiler.com/javascript
+
+// para corroborar las notaciones: https://www.dcode.fr/reverse-polish-notation
