e780d6b37d24d67ee55099dc017f90e11c96de95
Melany Holweger <melany.holweger@clara.co>
2021-09-18 17:55:11 -0300

    Both functions compile but still need changes

--- Ej2/testingFuncionesEnC.c
+++ Ej2/testingFuncionesEnC.c
@@ -9,6 +9,45 @@ Code, Compile, Run and Debug online from anywhere in world.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/*Definicion del stack Int*/
+
+typedef struct nodoInt{	
+    int info;
+	struct nodoInt* sgte;
+} nodoInt;
+
+void pushInt(nodoInt**, int);
+int popInt(nodoInt**);
+
+int popInt(nodoInt** pila){
+    int x;
+    nodoInt *p = *pila; 
+    if ( !p ){
+        return -1;
+    }
+    x = p->info;     
+    *pila = p->sgte;    
+    p->sgte = NULL; 
+    free( p );       
+    
+	return x;
+}
+
+
+void pushInt(nodoInt** pila, int x){	
+
+ nodoInt*p = malloc(sizeof (nodoInt));
+  p->info = x;	
+  p->sgte = *pila;	
+  *pila = p;		
+  return;
+}
+
+
+/*Fin Definicion stack Int*/
+
+
 /*Definicion del stack String*/
 
 typedef struct nodoString{	
@@ -53,7 +92,7 @@ int main()
     nodoString* stack = NULL;
     xd = obtenerNotacion(palabraAnalizada, &stack);
      for (int k = 0; k < strlen(palabraAnalizada); k++){
-                  printf("%s", pop(&xd));
+                  printf("%s", popString(&xd));
                 }
     return 0;
 }
@@ -123,3 +162,47 @@ void obtenerNotacion(char* palabraAnalizada, nodoString** stack){
   pushearOperador(operadorEnEspera, &stack);
 
 }
+
+
+// int main()
+// {
+//     //32*
+//     nodoString* stackNotacion = NULL;
+//     pushString(&stackNotacion, "3");
+//     pushString(&stackNotacion, "2");
+//     pushString(&stackNotacion, "*");
+    
+//     int resultado = evaluarNotacion(stackNotacion);
+
+//     return 0;
+// }
+
+
+int evaluarNotacion (nodoString** tokens) {
+    int i = 0;
+    nodoInt* stack = NULL;
+    char* nodoActual = popString(&tokens);
+    int operador1 = 0;
+    int operador2 = 0;
+
+    while(nodoActual != "-1") {
+        if (nodoActual == '+' || nodoActual == '*' || nodoActual == '-') {
+          operador2 = popInt(&stack);
+          operador1 = popInt(&stack);
+          pushInt(&stack, evaluar(operador1, operador2, nodoActual));
+        } else {
+          pushInt(&stack, atoi(nodoActual));
+        }
+        nodoActual = popString(&tokens);
+  }
+
+  return popInt(&stack);
+};
+
+int evaluar(n1, n2, operador){
+  if (operador == '+') return n1 + n2;
+  if (operador == '-') return n1 - n2;
+  if (operador == '*') return n1 * n2;
+};
+
+
